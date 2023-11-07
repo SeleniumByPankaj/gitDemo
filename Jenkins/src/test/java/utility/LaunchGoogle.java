474b37249a9b04c2ebd3cfe77e48a8abc6dcd39f
Pankaj <pankajbharambe101@gmail.com>
2023-11-07 23:49:36 +0530

    adding

---
+++ Jenkins/src/test/java/utility/LaunchGoogle.java
@@ -0,0 +1,24 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchGoogle {
+
+	@Test
+	public void LaunchBrowser() {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
+		String pageName = driver.getTitle();
+
+		System.out.println("Title of the page is " + pageName);
+
+	}
+}
