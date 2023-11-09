81578e8cbfe821b4b9caba6575fb456df06ccef5
Pankaj <pankajbharambe101@gmail.com>
2023-11-09 22:05:55 +0530

    adding sub method

--- Calculator/src/test/java/utility/BasicCalculator.java
+++ Calculator/src/test/java/utility/BasicCalculator.java
@@ -4,12 +4,22 @@ import org.testng.annotations.Test;
 
 public class BasicCalculator {
 
-	@Test
+	@Test (priority = 1)
 	public void sum() {
 		
 		int a = 5;
 		int b = 6;
 		int sum = a+b;
 		System.out.println("addition of two numbers is "+sum);
+	}
+	
+
+	@Test (priority = 2)
+	public void subtraction() {
+		
+		int a = 15;
+		int b = 6;
+		int sub = a-b;
+		System.out.println("subtraction of two numbers is "+sub);
 	}
 }
